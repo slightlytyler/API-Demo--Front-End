23a9c662d2673c8325cfa754955cc2b45f66a2cc
Tyler Martinez <slightlytyler@gmail.com>
2014-12-14 19:01:03 -0600

    Added contact(s) routes and main navigation

--- app/router.js
+++ app/router.js
@@ -6,6 +6,9 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.resource('contacts', function() {
+    this.resource('contact', { path: '/:contact_id' });
+  });
 });
 
 export default Router;
